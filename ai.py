ae20eccae56366a0b811182a6d1db327897c5a1f
dCaples <diegocaples@gmail.com>
2020-08-15 21:31:06 -0400

    first started working on the nuron class

--- ai.py
+++ ai.py
@@ -1,1 +1,18 @@
-print("hello world")
+import math
+#nuron class
+class node:
+    def __init__(self):
+        self.bias = 0.5
+        self.value = 0
+    def calc_output(self, input)
+        biased_input = input*self.bias 
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
