e9a9186b814fba637d2374d488c0f6d0920bf520
dCaples <diegocaples@gmail.com>
2020-08-18 12:03:20 -0400

    mostly done with layer

--- ai.py
+++ ai.py
@@ -1,14 +1,14 @@
 import random
 import math
 #nuron class
-class Node:
+class Nuron:
     def __init__(self):
         self.bias = 0.5
         self.value = 0
     def calc_output(self, input):
         biased_input = input*self.bias
         self.value = self.sigmoid(biased_input)
-        print(self.value)
+        return(self.value)
     def sigmoid(self, x):
         return 1 / (1 + math.exp(-x))
     def randomize(self, max_change):
@@ -17,7 +17,23 @@ class Node:
 
 class Layer:
     def __init__(self, layer_size):
+        self.layer_total = 0
+        self.nurons_in_layer = []
         for x in range(layer_size):
-            print(x)
-TestLayer = Layer(3)
+            self.nurons_in_layer.append(Nuron())
+    def get_layer_total(self, layer_input):
+        self.layer_total = 0
+        for x in range(len(self.nurons_in_layer)):
+            self.layer_total = self.layer_total+self.nurons_in_layer[x].calc_output(layer_input)
+        return(self.layer_total)
 
+    def randomize_layer(self, max_change):
+        for x in range(len(self.nurons_in_layer)):
+            self.nurons_in_layer[x].randomize(max_change)
+
+TestLayer = Layer(5)
+TestLayer.randomize_layer(.5)
+print(TestLayer.get_layer_total(10))
+TestNode = Nuron()
+TestNode.bias = -10
+print(TestNode.calc_output(1))
