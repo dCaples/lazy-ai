7743e92ae80291fed77d6e7879423732daa39e86
dCaples <diegocaples@gmail.com>
2020-08-17 20:51:05 -0400

    finished up node, started working on layer. should rename node to nuron

--- ai.py
+++ ai.py
@@ -1,12 +1,12 @@
 import random
 import math
 #nuron class
-class node:
+class Node:
     def __init__(self):
         self.bias = 0.5
         self.value = 0
     def calc_output(self, input):
-        biased_input = input*self.bias 
+        biased_input = input*self.bias
         self.value = self.sigmoid(biased_input)
         print(self.value)
     def sigmoid(self, x):
@@ -14,5 +14,10 @@ class node:
     def randomize(self, max_change):
         change = random.uniform(max_change*-1,max_change)
         self.bias = self.bias + change
-nuron = node()
-nuron.randomize(3)
+
+class Layer:
+    def __init__(self, layer_size):
+        for x in range(layer_size):
+            print(x)
+TestLayer = Layer(3)
+
