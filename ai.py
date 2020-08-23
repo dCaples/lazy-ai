1753a09ccf1a6adf0da232128c81d7fb3241d1ad
dCaples <diegocaples@gmail.com>
2020-08-22 21:28:03 -0400

    fixed syntax so it made more sense

--- ai.py
+++ ai.py
@@ -1,6 +1,6 @@
 import random
 import math
-class Nuron:
+class Node:
     def __init__(self):
         self.bias = 0.5
         self.value = 0
@@ -19,45 +19,45 @@ class Nuron:
 class Layer:
     def __init__(self, layer_size):
         self.layer_total = 0
-        self.nurons_in_layer = []
+        self.nodes_in_layer = []
         for x in range(layer_size):
-            self.nurons_in_layer.append(Nuron())
+            self.nodes_in_layer.append(Node())
     def get_layer_total(self, layer_input):
         self.layer_total = 0
-        for x in range(len(self.nurons_in_layer)):
-            self.layer_total = self.layer_total+self.nurons_in_layer[x].calc_output(layer_input)
+        for x in range(len(self.nodes_in_layer)):
+            self.layer_total = self.layer_total+self.nodes_in_layer[x].calc_output(layer_input)
         return(self.layer_total)
     def get_layer_total_from_array(self, input_array):
         self.layer_total = 0
         for x in range(len(input_array)):
-            self.nurons_in_layer[x].calc_output_no_sigmoid(input_array[x])
-            self.layer_total = self.layer_total+self.nurons_in_layer[x].value
+            self.nodes_in_layer[x].calc_output_no_sigmoid(input_array[x])
+            self.layer_total = self.layer_total+self.nodes_in_layer[x].value
         return(self.layer_total)
     def randomize_layer(self, max_change):
-        for x in range(len(self.nurons_in_layer)):
-            self.nurons_in_layer[x].randomize(max_change)
+        for x in range(len(self.nodes_in_layer)):
+            self.nodes_in_layer[x].randomize(max_change)
     def get_largest_output(self):
-        largest_output = self.nurons_in_layer[0]
+        largest_output = self.nodes_in_layer[0]
         largest_output_position = 0
-        for x in range(len(self.nurons_in_layer)):
-            if(self.nurons_in_layer[x].value>largest_output.value):
-                largest_output = self.nurons_in_layer[x]
+        for x in range(len(self.nodes_in_layer)):
+            if(self.nodes_in_layer[x].value>largest_output.value):
+                largest_output = self.nodes_in_layer[x]
                 largest_output_position = x
         return(largest_output_position)
     def print_data(self):
         print("printing outputs!")
-        for x in range (len(self.nurons_in_layer)):
-            print("the value of ", x,"is ", self.nurons_in_layer[x].value)
-            print(x, self.nurons_in_layer[x].value)
+        for x in range (len(self.nodes_in_layer)):
+            print("the value of ", x,"is ", self.nodes_in_layer[x].value)
+            print(x, self.nodes_in_layer[x].value)
 
-class Brain:
+class Network:
     def __init__(self, layers_to_create):
         self.reward = 0
         self.layers_array = []
         self.output = 0
         for x in range(len(layers_to_create)):
             self.layers_array.append(Layer(layers_to_create[x]))
-    def randomize_brain(self, max_change):
+    def randomize_network(self, max_change):
         for x in range(len(self.layers_array)):
             self.layers_array[x].randomize_layer(max_change)
     def print_layers(self):
@@ -74,9 +74,9 @@ class Brain:
         self.output = self.layers_array[-1].get_largest_output()
         return self.output
 class Evolution:
-    def __init__(self, layers_to_create, brains_in_evolution):
-        for x in range(brains_in_evolution):
+    def __init__(self, layers_to_create, networks_in_evolution):
+        for x in range(networks_in_evolution):
             print("hello:")
-BrainTest = Brain([3,3,3])
-BrainTest.randomize_brain(0.5)
-print(BrainTest.calculate([1,0,0]))
+NetworkTest = Network([3,3,3])
+NetworkTest.randomize_network(0.5)
+print(NetworkTest.calculate([1,0,0]))
